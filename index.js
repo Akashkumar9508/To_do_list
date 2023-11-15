9ff026e6ea0f24d8592438b11f675b5714043a74
Anant kumar <anant1042005@gmail.com>
2023-11-15 15:43:31 +0530

    indentation corrected

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const inputTask=document.getElementById("input-task");
-let listBox=document.getElementById("list-box");
-function addTask(){
-if(inputTask.value ==='')
-alert("please write somthing!");
-else{
-let li =document.createElement("li");
-li.innerHTML =inputTask.value;
-listBox.appendChild(li);
-let span =document.createElement("span");
-span.innerHTML="\u00d7";
-li.appendChild(span);
+const inputTask = document.getElementById("input-task");
+let listBox = document.getElementById("list-box");
+function addTask() {
+  if (inputTask.value === "") alert("please write somthing!");
+  else {
+    let li = document.createElement("li");
+    li.innerHTML = inputTask.value;
+    listBox.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputTask.value = "";
 }
-inputTask.value="";
-}
-listBox.addEventListener("click", function(e){
-if(e.target.tagName === "LI"){
-e.target.classList.toggle("check");
-}
-else if (e.target.tagName === "SPAN"){
-e.target.parentElement.remove();
-}
-}, false);
+listBox.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("check");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
